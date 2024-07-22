0060ae8eecdd5c50847ec785a5552b04c9ac5be0
Fabio Ramos <fabiowilliam466@gmail.com>
2024-07-22 08:58:21 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template, send_file
+import qrcode
+import io
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate_qr():
+    url = request.form.get('url')
+    img = qrcode.make(url)
+    buf = io.BytesIO()
+    img.save(buf)
+    buf.seek(0)
+    return send_file(buf, mimetype='image/png', as_attachment=True, attachment_filename='qrcode.png')
+
+if __name__ == '__main__':
+    app.run(debug=True)
